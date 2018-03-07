7717d47fce8edf1cb5bf4fd39a9b4987da973059
sampbay <sampbay@outlook.com>
2018-03-07 03:38:00 -0500

    fixed profile - form_for

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,6 @@
 class ProfilesController < ApplicationController
-	before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
-	before_action :check_profile_presence, only: [:new, :create]
+	#before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
+	#before_action :check_profile_presence, only: [:new, :create]
 	#before_action :require_user_profile_exists, only: [:edit, :update, :destroy]
 	#before_action :require_user_profile_nil, only: [:new, :create]
 	def welcome; end
@@ -53,7 +53,7 @@ class ProfilesController < ApplicationController
 		params.require(:profile).permit(:age, :skin_type, :ethnicity, :user)
 	end
 
-	def check_profile_presence
-		redirect_to current_user if Profile.exists?(current_user.profile)
-	end
+#	def check_profile_presence
+#		redirect_to current_user if Profile.exists?(current_user.profile)
+#	end
 end
