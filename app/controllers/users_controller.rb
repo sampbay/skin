e8a39ef88a8ac09161546aa8b196c9f604d44e3c
sampbay <sampbay@outlook.com>
2018-03-10 20:33:01 -0500

    users show edit if else

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,10 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		#rescue ActiveRecord::RecordNotFound
+		
 		# @user = User.find(params[:id])
 		@user = current_user
+		rescue ActiveRecord::RecordNotFound
 		# Skin Profile - acts as profiles_controller index
 		@profile = Profile.find(current_user.profile)
 		
