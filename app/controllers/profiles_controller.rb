943aa5917a15ce00d2096ef604dafbb2f0901a28
sampbay <sampbay@outlook.com>
2018-03-12 02:02:56 -0400

    added profile_params to new profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,21 @@
 class ProfilesController < ApplicationController
-	#before_action :require_user, only: [:index, :new, :create, :edit, :update, :destroy]
-	#before_action :check_profile_presence, only: [:new, :create]
+	before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
+	before_action :check_profile_presence, only: [:new, :create]
 	#before_action :require_user_profile_exists, only: [:edit, :update, :destroy]
 	#before_action :require_user_profile_nil, only: [:new, :create]
 	def welcome; end
-	def index
-		@profile = Profile.find(current_user.profile)
+	#def index
+	#	@profile = Profile.find(current_user.profile)
 	#	@myproduct = Myproduct.find(current_user.myproducts)
 		# @parsed_products = JSON.parse(@profile.products)
-		rescue ActiveRecord::RecordNotFound
-	end	
+	#	rescue ActiveRecord::RecordNotFound
+	#end	
 	#def show
 	#	@profile = Profile.find(params[:id])
 		# show ingredients by ingredients controller
 	#end
 	def new
-		@profile = Profile.new
+		@profile = Profile.new(profile_params)
 	    # 3.times { @profile.products.build }
 	    # @product_search = Product.find_by_sql "SELECT row_to_json(row(name, brand)) FROM products"
 		
